998b90d63ed58bfade8666253890a50423ac2a73
tongchaitonsau <ais_arm@hotmail.com>
2017-09-15 11:00:26 +0700

    add function onClick and dialog

--- app/src/main/java/com/example/tongchaitonsau/smartsmallshowroom/MainActivity.java
+++ app/src/main/java/com/example/tongchaitonsau/smartsmallshowroom/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.tongchaitonsau.smartsmallshowroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        Button item1 = (Button) findViewById(R.id.item1_i);
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent purchase = new Intent(MainActivity.this,PurchaseActivity.class);
+                startActivity(purchase);
+
+            }
+        });
+    }
+
+    private void init(){
+
     }
 }
--- app/src/main/java/com/example/tongchaitonsau/smartsmallshowroom/PurchaseActivity.java
+++ app/src/main/java/com/example/tongchaitonsau/smartsmallshowroom/PurchaseActivity.java
@@ -1,7 +1,10 @@
 package com.example.tongchaitonsau.smartsmallshowroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PurchaseActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class PurchaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+
+        Button purchase = (Button) findViewById(R.id.purchase_i);
+        purchase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent paymoney = new Intent(PurchaseActivity.this,GetmoneyActivity.class);
+                startActivity(paymoney);
+            }
+        });
     }
 }
